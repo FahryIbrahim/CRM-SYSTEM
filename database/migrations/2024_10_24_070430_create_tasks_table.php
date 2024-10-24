fdb371a7322d917bfc0340b7c83b90baad4d8926
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-10-24 15:02:55 +0700

    feat(task): create migration for tasks table

--- database/migrations/2024_10_24_070430_create_tasks_table.php
+++ database/migrations/2024_10_24_070430_create_tasks_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->dateTime('due_date');
             $table->timestamps();
         });
     }
