f080af9ade6384d89c4de5bcc5fe60e7777778f7
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-10-25 13:01:48 +0700

    feat(task): create fillable on Task Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'project_id',
+        'user_id',
+        'status',
+        'due_date'
+    ];
 }
